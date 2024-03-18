3e6eff9f49459fc79af2c6bb5fa4f0b455eccff3
leonil10 <chipana.leo1@gmail.com>
2024-03-18 17:21:05 -0500

    estructure of ms-ticket

---
+++ src/test/java/com/grupo9/mstikect/MsTikectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo9.mstikect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTikectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
